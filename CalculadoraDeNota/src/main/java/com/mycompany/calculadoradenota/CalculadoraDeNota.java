1b3668ae1f0dc3cecd00e955757b19f65062753d
SergioAlexandre1 <159459950+SergioAlexandre1@users.noreply.github.com>
2025-03-13 10:41:08 -0300

    Add files via upload

---
+++ CalculadoraDeNota/src/main/java/com/mycompany/calculadoradenota/CalculadoraDeNota.java
@@ -0,0 +1,14 @@
+package com.mycompany.calculadoradenota;
+import java.util.Scanner;
+public class CalculadoraDeNota {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello World!");
+        // Pelo menos 2 construtores.
+        // Get/Set
+        // Imprimir – Imprime os dados do aluno de forma formatada.
+        // Passar – Modifica turma e período.
+        // Calcular Final – Calcula a média das notas do aluno. 
+    }
+}
